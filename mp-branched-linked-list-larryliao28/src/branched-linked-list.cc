a51ef352d56ae5636c83ec3eac1ccc22b629b51b
Larry Liao <68320107+larryliao28@users.noreply.github.com>
2022-04-05 21:28:21 -0500

    Add files via upload

--- mp-branched-linked-list-larryliao28/src/branched-linked-list.cc
+++ mp-branched-linked-list-larryliao28/src/branched-linked-list.cc
@@ -43,17 +43,24 @@ BLL::BLL(const BLL& to_copy) {
     head_ = nullptr;
     return;
   }
-  head_ = new Node(to_copy.head_->data_);
-  auto* prev = head_;
-  auto* current = to_copy.head_->next_node_;
-  while (current != nullptr) {
-    auto* node = new Node(current->data_);
-    prev->next_node_ = node;
-    prev = node;
-    if (current->next_bll_ != nullptr) {
-      BLL(*(current->next_bll_));
+  // head_ = new Node(to_copy.head_->data_);
+  Node* prev = nullptr;
+  Node* curr = nullptr;        // current
+  auto* copy = to_copy.head_;  // current_copy
+  bool first_check = true;
+  while (copy != nullptr) {
+    prev = curr;
+    curr = new Node(copy->data_);
+    if (first_check) {
+      head_ = curr;
     }
-    current = current->next_node_;
+    if (prev != nullptr) {
+      prev->next_node_ = curr;
+    }
+    if (curr->next_bll_ != nullptr) {
+      curr->next_bll_ = new BLL(*(copy->next_bll_));
+    }
+    copy = curr->next_node_;
   }
 }
 
@@ -67,17 +74,23 @@ BLL& BLL::operator=(const BLL& rhs) {
     head_ = nullptr;
     return *this;
   }
-  head_ = new Node(rhs.head_->data_);
-  auto* prev = head_;
-  auto* current = rhs.head_->next_node_;
-  while (current != nullptr) {
-    auto* node = new Node(current->data_);
-    prev->next_node_ = node;
-    prev = node;
-    if (current->next_bll_ != nullptr) {
-      BLL(*(current->next_bll_));
+  Node* prev = nullptr;
+  Node* curr = nullptr;
+  auto* copy = rhs.head_;
+  bool first_check = true;
+  while (copy != nullptr) {
+    prev = curr;
+    curr = new Node(copy->data_);
+    if (first_check) {
+      head_ = curr;
     }
-    current = current->next_node_;
+    if (prev != nullptr) {
+      prev->next_node_ = curr;
+    }
+    if (curr->next_bll_ != nullptr) {
+      curr->next_bll_ = new BLL(*(copy->next_bll_));
+    }
+    copy = curr->next_node_;
   }
   return *this;
 }
@@ -118,25 +131,17 @@ char BLL::GetAt(size_t idx) const {
   }
   Node* current = head_;
   size_t tmp_idx = 0;
-  while (tmp_idx != idx) {
+  while (tmp_idx < idx) {
     if (current->next_bll_ != nullptr) {
-      std::cout << current->data_;
-      if ((current->next_bll_->Size()) < (Size() - tmp_idx - 1)) {  // 3 <= 5
-        tmp_idx += current->next_bll_->Size();  // tmp_idx = 3
+      if ((current->next_bll_->Size() + tmp_idx) < idx) {
+        tmp_idx += current->next_bll_->Size();
       } else {
         return current->next_bll_->GetAt(idx - tmp_idx - 1);
       }
-      // return GetAt(idx - 1);
     }
     current = current->next_node_;
-    if (current == nullptr) {
-      std::cout << " error";
-      return 'a';
-    }
     tmp_idx++;
-    std::cout << tmp_idx;
   }
-  // current->next_bll_->Size()
 
   return current->data_;
 }
@@ -148,9 +153,13 @@ void BLL::SetAt(size_t idx, char dat) {
   }
   Node* current = head_;
   size_t tmp_idx = 0;
-  while (tmp_idx != idx) {
+  while (tmp_idx < idx) {
     if (current->next_bll_ != nullptr) {
-      current = current->next_bll_->head_;
+      if ((current->next_bll_->Size() + tmp_idx) < idx) {
+        tmp_idx += current->next_bll_->Size();
+      } else {
+        return current->next_bll_->SetAt(idx - tmp_idx - 1, dat);
+      }
     }
     current = current->next_node_;
     tmp_idx++;
@@ -174,12 +183,24 @@ std::string BLL::ToStringHelper(Node* node) const {
 // detect cyclic, not complete
 bool BLL::IsBLLAcyclic() const { return IsBLLAcyclicHelp(head_); }
 bool BLL::IsBLLAcyclicHelp(Node* node) const {
-  if (node == nullptr) {
+  if (node == nullptr || node->next_node_ == nullptr) {
+    std::cout << "hit the end" << std::endl;
     return true;
   }
   Node* turtle = node;
   Node* hare = node->next_node_;
-  while (turtle != nullptr && hare != nullptr && hare->next_node_ != nullptr) {
+
+  std::cout << "before while loop" << std::endl;
+  std::cout << "turtle: " << turtle->data_ << std::endl;
+  std::cout << "hare: " << hare->data_ << std::endl;
+
+  while ((turtle != nullptr || turtle->next_bll_ != nullptr) &&
+         (hare != nullptr || hare->next_bll_ != nullptr) &&
+         (hare->next_node_ != nullptr || hare->next_bll_ != nullptr)) {
+    std::cout << "in while loop" << std::endl;
+    std::cout << "turtle: " << turtle->data_ << std::endl;
+    std::cout << "hare: " << hare->data_ << std::endl;
+
     if (turtle == hare) {
       return false;
     }
@@ -189,9 +210,17 @@ bool BLL::IsBLLAcyclicHelp(Node* node) const {
     if (turtle->next_bll_ != nullptr) {
       return IsBLLAcyclicHelp(turtle->next_bll_->head_);
     }
+
     turtle = turtle->next_node_;
-    hare = hare->next_node_->next_node_;
+    if (hare != nullptr && hare->next_node_ != nullptr &&
+        hare->next_node_->next_node_ != nullptr) {
+      hare = hare->next_node_->next_node_;
+    } else {
+      std::cout << "doesn't find the cycle 1" << std::endl;
+      return true;
+    }
   }
+  std::cout << "doesn't find the cycle 2" << std::endl;
   return true;
 }
 
@@ -203,11 +232,22 @@ void BLL::Join(size_t idx, BLL* list) {
   Node* current = head_;
   size_t tmp_idx = 0;
   while (tmp_idx != idx) {
-    // if (current->next_bll_ != nullptr) {
-    //   current = current->next_bll_->head_;
-    // }
+    if (current->next_bll_ != nullptr) {
+      if ((current->next_bll_->Size() + tmp_idx) < idx) {
+        tmp_idx += current->next_bll_->Size();
+      } else {
+        return current->next_bll_->Join(idx - tmp_idx - 1, list);
+      }
+    }
     current = current->next_node_;
     tmp_idx++;
   }
+  if (current->next_bll_ != nullptr) {
+    throw std::runtime_error("branch already exist");
+  }
+  // also need to test for cyclic
   current->next_bll_ = list;
+  if (!IsBLLAcyclic()) {
+    throw std::runtime_error("cycle behavior");
+  }
 }
--- mp-branched-linked-list-larryliao28/src/driver.cc
+++ mp-branched-linked-list-larryliao28/src/driver.cc
@@ -28,23 +28,25 @@ int main() {
   // std::cout << second->ToString() << std::endl;  // i!@
   // std::cout << second->Size() << std::endl;      // 3
 
-  BLL* third = new BLL();
-  third->PushBack('l');
-  third->PushBack('a');
-  third->PushBack('r');
-  // first -> = new Node();
-  first->Join(1, third);  // h -> lar -> e , first: he, third: lar
-  std::cout << first->ToString() << std::endl;
-  std::cout << first->Size() << std::endl;
-  std::cout << first->GetAt(2) << std::endl;
+  // BLL* third = new BLL();
+  // third->PushBack('l');
+  // third->PushBack('a');
+  // third->PushBack('r');
+  // first->Join(0, third);  // h -> lar -> e , first: he, third: lar
+  // std::cout << first->ToString() << std::endl;
+  // std::cout << first->Size() << std::endl;
+  // std::cout << first->GetAt(1) << std::endl;
+  // first->SetAt(4, 'x');
+  // std::cout << first->ToString() << std::endl;
   // std::cout << first->IsBLLAcyclic() << std::endl;
 
-  // BLL* four = new BLL();
-  // four->PushBack('E');
-  // first->Join(1, four);
-  // four->Join(0, first);
-  // std::cout << four->ToString() << std::endl;
-  // std::cout << first->IsBLLAcyclic() << std::endl;
+  BLL* four = new BLL();
+  four->PushBack('E');
+  first->Join(1, four);
+  std::cout << four->ToString() << std::endl;
+  four->Join(0, first);
+  // std::cout << first->ToString() << std::endl;
+  std::cout << first->IsBLLAcyclic() << std::endl;
   // BLL* four;
   // four = first;
   // std::cout << four->ToString() << std::endl;
@@ -53,19 +55,19 @@ int main() {
 
   // first->Join(1, second);
 
-  // std::cout << first->ToString() << std::endl; // hii!@
-  // std::cout << first->Size() << std::endl; // 5
+  // std::cout << first->ToString() << std::endl;  // hii!@
+  // std::cout << first->Size() << std::endl;      // 5
 
-  // std::cout << second->ToString() << std::endl; // i!@
-  // std::cout << second->Size() << std::endl; // 3
+  // std::cout << second->ToString() << std::endl;  // i!@
+  // std::cout << second->Size() << std::endl;      // 3
 
   // /////////////////////////////////////
 
   // BLL* first_clone = new BLL();
-  // *first_clone = *first;
+  // first_clone = first;
 
-  // std::cout << first_clone->ToString() << std::endl; // hii!@
-  // std::cout << first_clone->Size() << std::endl; // 5
+  // std::cout << first_clone->ToString() << std::endl;  // hii!@
+  // std::cout << first_clone->Size() << std::endl;      // 5
 
   return 0;
 }
